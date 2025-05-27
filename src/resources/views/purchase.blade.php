65a9938a6eeafca5909498aac2e180e38535c4f6
hinako shimamori <isola16858foresta198401@gmail.com>
2025-05-27 22:21:55 +0900

    stripe設定追加

--- src/resources/views/purchase.blade.php
+++ src/resources/views/purchase.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 
+@section('head')
+<!-- stripe遷移用リンク -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+@endsection
+
 @section('css')
 <link rel="stylesheet" href="{{ asset('css/purchase.css') }}">
 @endsection
@@ -7,5 +12,6 @@
 @section('content')
 <div class="content">
     購入
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </div>
 @endsection

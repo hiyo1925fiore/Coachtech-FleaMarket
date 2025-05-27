8cd280860286d85bf0a49b8ac88c7a3247c81df9
hinako shimamori <isola16858foresta198401@gmail.com>
2025-05-27 22:21:55 +0900

    stripe設定追加

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -6,11 +6,12 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use Billable,HasApiTokens, HasFactory, Notifiable;
 
     public function profile()
     {
